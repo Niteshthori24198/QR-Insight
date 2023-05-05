8066393fd1ab1884e08a8143a7697ce9f9f569c2
Nitesh Kumar <niteshkumar240198@gmail.com>
2023-05-05 17:10:43 +0530

    wifi-qr-frontend-nitesh

--- Front-End/Scripts/qr.js
+++ Front-End/Scripts/qr.js
@@ -141,10 +141,11 @@ const QR_Code_img_email = document.getElementById('QR_Code_img_email');
 
 const QR_Code_img_zoom = document.getElementById('QR_Code_img_zoom');
 
-// **************************************************************
-//    Text QR Code Generator
+const QR_Code_img_wifi = document.getElementById('QR_Code_img_wifi');
 
 
+
+// Text QR Code Generator
 const text_qr_form = document.getElementById('text_qr_form');
 
 text_qr_form.addEventListener("submit", async (event) => {
@@ -152,16 +153,15 @@ text_qr_form.addEventListener("submit", async (event) => {
     event.preventDefault();
 
     const msg = text_qr_form['text-message-qr'].value.trim()
-    
-    // console.log(msg);
 
-    if( !msg ){
+
+    if (!msg) {
         alert(`Please write a valid infomation ! `);
         return
     }
 
 
-    if( !QrColorDesign ) QrColorDesign = "black"
+    if (!QrColorDesign) QrColorDesign = "black"
 
 
     QR_Code_img_text.src = `https://cdn.dribbble.com/users/964000/screenshots/3675038/switcher_qr_barcode_alinashi_shot.gif`;
@@ -169,255 +169,197 @@ text_qr_form.addEventListener("submit", async (event) => {
 
 
     const payload = {
-        text : msg,
-        color : QrColorDesign
+        text: msg,
+        color: QrColorDesign
     }
 
 
     const res = await fetch(`${QR_BASE_URL}/text`, {
-        method : "POST",
-        headers : {
-            "Content-type" : "application/json"
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
         },
-        body : JSON.stringify(payload)
-    }).then(res => res.json()).catch(err=>{
+        body: JSON.stringify(payload)
+    }).then(res => res.json()).catch(err => {
         console.log(err);
     })
 
 
-    // console.log(res);
-
-    setTimeout(()=>{
+    setTimeout(() => {
 
         QR_Code_img_text.src = res.qrcode;
 
-    },5000);
-
-    
-    // console.log(QrColorDesign);
-
+    }, 5000);
 
 })
 
 
 //    Link QR Code Generator
-
-
 const link_qr_form = document.getElementById('link_qr_form');
 
 link_qr_form.addEventListener("submit", async (event) => {
 
     event.preventDefault();
 
-    // console.log('clicked');
-
     const link = link_qr_form['link-url-qrl'].value.trim()
-    
-    // console.log(link);
 
-    if( !link ){
+    if (!link) {
         alert(`Please write a valid infomation ! `);
         return
     }
 
-
-    if( !QrColorDesign ) QrColorDesign = "black"
+    if (!QrColorDesign) QrColorDesign = "black"
 
 
     QR_Code_img_link.src = `https://cdn.dribbble.com/users/964000/screenshots/3675038/switcher_qr_barcode_alinashi_shot.gif`;
 
 
-
     const payload = {
-        URL : link,
-        color : QrColorDesign
+        URL: link,
+        color: QrColorDesign
     }
 
 
     const res = await fetch(`${QR_BASE_URL}/link`, {
-        method : "POST",
-        headers : {
-            "Content-type" : "application/json"
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
         },
-        body : JSON.stringify(payload)
-    }).then(res => res.json()).catch(err=>{
+        body: JSON.stringify(payload)
+    }).then(res => res.json()).catch(err => {
         console.log(err);
     })
 
 
-    // console.log(res);
-
-    setTimeout(()=>{
+    setTimeout(() => {
 
         QR_Code_img_link.src = res.qrcode;
 
-    },5000);
-
-    
-    // console.log(QrColorDesign);
-
+    }, 5000);
 
 })
 
 
 // Phone QR Generator
-
-
-
 const phone_qr_form = document.getElementById('phone_qr_form');
 
 phone_qr_form.addEventListener("submit", async (event) => {
 
     event.preventDefault();
 
-    // console.log('clicked');
-
     const countrycode = phone_qr_form['select_country_code_phone'].value.trim()
 
     const phoneNumber = phone_qr_form['phone-number-qrl'].value.trim()
-    
-    // console.log(link);
 
-    if( !countrycode || !phoneNumber ){
+
+    if (!countrycode || !phoneNumber) {
         alert(`Please write a valid infomation ! `);
         return
     }
 
 
-    if( !QrColorDesign ) QrColorDesign = "black"
+    if (!QrColorDesign) QrColorDesign = "black"
 
 
     QR_Code_img_phone.src = `https://cdn.dribbble.com/users/964000/screenshots/3675038/switcher_qr_barcode_alinashi_shot.gif`;
 
 
-
     const payload = {
-        phone : `${countrycode}${phoneNumber}`,
-        color : QrColorDesign
+        phone: `${countrycode}${phoneNumber}`,
+        color: QrColorDesign
     }
 
-    console.log(payload)
 
     const res = await fetch(`${QR_BASE_URL}/phone`, {
-        method : "POST",
-        headers : {
-            "Content-type" : "application/json"
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
         },
-        body : JSON.stringify(payload)
-    }).then(res => res.json()).catch(err=>{
+        body: JSON.stringify(payload)
+    }).then(res => res.json()).catch(err => {
         console.log(err);
     })
 
 
-    // console.log(res);
-
-    setTimeout(()=>{
+    setTimeout(() => {
 
         QR_Code_img_phone.src = res.qrcode;
 
-    },5000);
-
-    
-    // console.log(QrColorDesign);
-
+    }, 5000);
 
 })
 
 
 // QR for Whatsapp
-
-
 const whatsapp_qr_form = document.getElementById('whatsapp_qr_form');
 
 whatsapp_qr_form.addEventListener("submit", async (event) => {
 
     event.preventDefault();
 
-    // console.log('clicked');
-
     const countrycode = whatsapp_qr_form['select_country_code_wa'].value.trim()
 
     const phoneNumber = whatsapp_qr_form['wa-number-qrl'].value.trim()
 
     const Message = whatsapp_qr_form['wa-text-message-qr'].value.trim()
-    
-    // console.log(link);
 
-    if( !countrycode || !phoneNumber){
+
+    if (!countrycode || !phoneNumber) {
         alert(`Please write a valid infomation ! `);
         return
     }
 
-
-    if( !QrColorDesign ) QrColorDesign = "black"
+    if (!QrColorDesign) QrColorDesign = "black"
 
 
     QR_Code_img_wa.src = `https://cdn.dribbble.com/users/964000/screenshots/3675038/switcher_qr_barcode_alinashi_shot.gif`;
 
-
-
     const payload = {
-        phone : `${countrycode}${phoneNumber}`,
-        text : Message,
-        color : QrColorDesign
+        phone: `${countrycode}${phoneNumber}`,
+        text: Message,
+        color: QrColorDesign
     }
 
-    console.log(payload);
-
-    console.log(payload)
-
     const res = await fetch(`${QR_BASE_URL}/whatsapp`, {
-        method : "POST",
-        headers : {
-            "Content-type" : "application/json"
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
         },
-        body : JSON.stringify(payload)
-    }).then(res => res.json()).catch(err=>{
+        body: JSON.stringify(payload)
+    }).then(res => res.json()).catch(err => {
         console.log(err);
     })
 
 
-    // console.log(res);
-
-    setTimeout(()=>{
+    setTimeout(() => {
 
         QR_Code_img_wa.src = res.qrcode;
 
-    },5000);
-
-    
-    // console.log(QrColorDesign);
-
-
+    }, 5000);
 })
 
 
 // QR generation for UPI
-
 const upi_qr_form = document.getElementById('upi_qr_form')
 
 upi_qr_form.addEventListener("submit", async (event) => {
 
     event.preventDefault();
 
-    // console.log('clicked');
-
     const name = upi_qr_form['upi-id-name'].value.trim()
 
     const upi = upi_qr_form['upi-id-qrl'].value.trim()
 
     const amount = upi_qr_form['upi-id-amount-qrl'].value.trim()
-    
-    
-   
 
-    if( !name || !upi){
+
+    if (!name || !upi) {
         alert(`Please write a valid infomation ! `);
         return
     }
 
 
-    if( !QrColorDesign ) QrColorDesign = "black"
+    if (!QrColorDesign) QrColorDesign = "black"
 
 
     QR_Code_img_upi.src = `https://cdn.dribbble.com/users/964000/screenshots/3675038/switcher_qr_barcode_alinashi_shot.gif`;
@@ -425,37 +367,28 @@ upi_qr_form.addEventListener("submit", async (event) => {
 
 
     const payload = {
-        name : name,
-        upi : upi,
-        amount : amount,
-        color : QrColorDesign
+        name: name,
+        upi: upi,
+        amount: amount,
+        color: QrColorDesign
     }
 
-    console.log(payload)
-
     const res = await fetch(`${QR_BASE_URL}/upi`, {
-        method : "POST",
-        headers : {
-            "Content-type" : "application/json"
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
         },
-        body : JSON.stringify(payload)
-    }).then(res => res.json()).catch(err=>{
+        body: JSON.stringify(payload)
+    }).then(res => res.json()).catch(err => {
         console.log(err);
     })
 
 
-    // console.log(res);
-
-    setTimeout(()=>{
+    setTimeout(() => {
 
         QR_Code_img_upi.src = res.qrcode;
 
-    },5000);
-
-    
-    // console.log(QrColorDesign);
-
-
+    }, 5000);
 })
 
 
@@ -475,17 +408,15 @@ email_qr_form.addEventListener("submit", async (event) => {
     const subject = email_qr_form['email_subject_qr'].value.trim()
 
     const body = email_qr_form['email-body-qr'].value.trim()
-    
-    
-   
 
-    if( !mailto ){
+
+    if (!mailto) {
         alert(`Please write a valid infomation ! `);
         return
     }
 
 
-    if( !QrColorDesign ) QrColorDesign = "black"
+    if (!QrColorDesign) QrColorDesign = "black"
 
 
     QR_Code_img_email.src = `https://cdn.dribbble.com/users/964000/screenshots/3675038/switcher_qr_barcode_alinashi_shot.gif`;
@@ -496,33 +427,26 @@ email_qr_form.addEventListener("submit", async (event) => {
         mailto,
         subject,
         body,
-        color : QrColorDesign
+        color: QrColorDesign
     }
 
     console.log(payload)
 
     const res = await fetch(`${QR_BASE_URL}/email`, {
-        method : "POST",
-        headers : {
-            "Content-type" : "application/json"
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
         },
-        body : JSON.stringify(payload)
-    }).then(res => res.json()).catch(err=>{
+        body: JSON.stringify(payload)
+    }).then(res => res.json()).catch(err => {
         console.log(err);
     })
 
-
-    // console.log(res);
-
-    setTimeout(()=>{
+    setTimeout(() => {
 
         QR_Code_img_email.src = res.qrcode;
 
-    },5000);
-
-    
-    // console.log(QrColorDesign);
-
+    }, 5000);
 
 })
 
@@ -542,52 +466,98 @@ zoom_qr_form.addEventListener("submit", async (event) => {
 
     const meetingLink = zoom_qr_form['zoom_meeting_url_qr'].value.trim()
 
-    
-    
-   
 
-    if( !meetingId && !meetingLink ){
+    if (!meetingId && !meetingLink) {
         alert(`Please write a valid infomation ! `);
         return
     }
 
 
-    if( !QrColorDesign ) QrColorDesign = "black"
+    if (!QrColorDesign) QrColorDesign = "black"
 
 
     QR_Code_img_zoom.src = `https://cdn.dribbble.com/users/964000/screenshots/3675038/switcher_qr_barcode_alinashi_shot.gif`;
 
 
-
     const payload = {
         meetingId,
         meetingLink,
-        color : QrColorDesign
+        color: QrColorDesign
     }
 
-    console.log(payload)
 
     const res = await fetch(`${QR_BASE_URL}/zoom`, {
-        method : "POST",
-        headers : {
-            "Content-type" : "application/json"
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
         },
-        body : JSON.stringify(payload)
-    }).then(res => res.json()).catch(err=>{
+        body: JSON.stringify(payload)
+    }).then(res => res.json()).catch(err => {
         console.log(err);
     })
 
 
-    console.log(res);
 
-    setTimeout(()=>{
+    setTimeout(() => {
 
         QR_Code_img_zoom.src = res.qrcode;
 
-    },5000);
+    }, 5000);
+})
 
-    
-    // console.log(QrColorDesign);
 
+
+
+// QR generation for Email
+
+const wifi_qr_form = document.getElementById('wifi_qr_form');
+
+wifi_qr_form.addEventListener("submit", async (event) => {
+
+    event.preventDefault();
+
+    const ssid = wifi_qr_form['wifi-ssid'].value.trim()
+
+    const encryption = wifi_qr_form['select_network_security_type'].value.trim()
+
+    const password = wifi_qr_form['wifi-password'].value.trim()
+
+
+    if (!ssid && !password) {
+        alert(`Please write a valid infomation ! `);
+        return
+    }
+
+
+    if (!QrColorDesign) QrColorDesign = "black"
+
+
+    QR_Code_img_wifi.src = `https://cdn.dribbble.com/users/964000/screenshots/3675038/switcher_qr_barcode_alinashi_shot.gif`;
+
+
+    const payload = {
+        ssid,
+        password,
+        encryption,
+        color: QrColorDesign
+    }
+
+
+    const res = await fetch(`${QR_BASE_URL}/wifi`, {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(payload)
+    }).then(res => res.json()).catch(err => {
+        console.log(err);
+    })
+
+
+    setTimeout(() => {
+
+        QR_Code_img_wifi.src = res.qrcode;
+
+    }, 5000);
 
 })
