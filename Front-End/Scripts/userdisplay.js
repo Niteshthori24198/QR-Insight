35b93c5dd929429ccd76a5be761c1ae9266dbfcf
Mrkishansharma <kishansharma6377@gmail.com>
2023-05-08 22:28:15 +0530

    blog page done

--- Front-End/Scripts/userdisplay.js
+++ Front-End/Scripts/userdisplay.js
@@ -9,20 +9,22 @@ let obj={
     _id:b
 }
 
+if(b){
+    fetch(`http://localhost:3000/user/getdata/?_id=${b}`)
+    .then((res)=>{
+        return res.json()
+    })
+    .then((user)=>{
+        userdetails=user
+        console.log(user.userdetails.Name)
+        localStorage.setItem("qrcodeuserdetails",JSON.stringify(user.userdetails))
+        
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
 
-fetch(`http://localhost:3000/user/getdata/?_id=${b}`)
-.then((res)=>{
-    return res.json()
-})
-.then((user)=>{
-    userdetails=user
-    console.log(user.userdetails.Name)
-    localStorage.setItem("qrcodeuserdetails",JSON.stringify(user.userdetails))
-    
-})
-.catch((err)=>{
-    console.log(err)
-})
+}
 
 
 
